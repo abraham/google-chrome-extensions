9aa59cca57ed16c9500a8bf25d7507dfc5d89e35
abraham <4braham@gmail.com>
2012-05-22 20:48:11 -0700

    added born to profile modal added caching, only add divider if url is present

--- born/background.js
+++ born/background.js
@@ -1,22 +1,40 @@
 chrome.extension.onRequest.addListener(handleUserId); 
 
 function handleUserId(request, sender, sendResponse) {
-  // TODO: check for cached data
+  // Return cached date if present
+  if (sessionStorage.getItem(getCacheName(request.userId))) {
+    var data = { createdAt: sessionStorage.getItem(getCacheName(request.userId)), userId: request.userId };
+    sendResponse({ status: 200, data: data });
+    return;
+  }
+  
   var xhr = new XMLHttpRequest();
   xhr.open("GET", "https://api.twitter.com/1/users/show.json?user_id=" + request.userId, true);
   xhr.send();
   xhr.onreadystatechange = function() {
     if (xhr.readyState == 4) {
-      // TODO: cache success data
-      sendResponse({ status: xhr.status, response: JSON.parse(xhr.responseText) });
+      var response = { status: xhr.status };
+      if (xhr.status === 200) {
+        var profile = JSON.parse(xhr.responseText);
+        response.createdAt = profile.created_at;
+        response.userId = profile.id;
+        sessionStorage.setItem(getCacheName(profile.id), profile.created_at);
+      }
+      sendResponse({ status: xhr.status, data: response });
     }
   }
 }
 
+// Diplay notification if new install
+// TODO: remove at some point in the future
 if (localStorage.getItem('installed') !== 'true') {
   var url = chrome.extension.getURL('cracked-twitter.png');
   var body = 'The extension Twitter Creation Date is now Born for Twitter and has been updated to work with #newtwitter.';
   var popup = window.webkitNotifications.createNotification(url, 'Twitter Creation Date Reborn', body);
   popup.show();
   localStorage.setItem('installed', 'true');
+}
+
+function getCacheName(id) {
+  return 'cache_' + id + '_created_at';
 }
--- born/content.js
+++ born/content.js
@@ -1,37 +1,52 @@
-// TODO: adds support for modals
 var observer = new MutationSummary({
   callback: handleProfileChange,
-  queries: [{ element: 'div.profile-card-inner' }]
+  queries: [
+    { element: 'div.profile-card-inner' },
+    { element: 'div.profile-modal' }
+  ]
 });
 
 function handleProfileChange(summaries) {
-  // TODO: loop through summaries?
-  var summary = summaries[0];
-
-  summary.added.forEach(function(profile) {
+  // Full profile
+  summaries[0].added.forEach(function(profile) {
     var userId = profile.getAttribute('data-user-id');
-    profile.className += ' born-profile-' + userId;
+    profile.className += ' ' + getProfileClassName(userId);
+    chrome.extension.sendRequest(null, { userId: userId },  handleResponse);
+  });
+  // Profile modal
+  summaries[1].added.forEach(function(modal) {
+    var userId = modal.getElementsByClassName('js-action-follow')[0].getAttribute('data-user-id');
+    modal.getElementsByClassName('profile-modal-extended')[0].className += ' ' + getProfileClassName(userId);
     chrome.extension.sendRequest(null, { userId: userId },  handleResponse);
   });
 }
 
 function handleResponse(response) {
   if (response.status === 200) {
-    // TODO: Clean up code
-    var d = document.createElement('span');
-    d.className = 'divider';
-    d.innerText = '·';
-    var profile = response.response;
-    var profilesOnPage = document.getElementsByClassName('born-profile-' + profile.id);
+    // TODO: clean up code
+    var profilesOnPage = document.getElementsByClassName(getProfileClassName(response.data.userId));
     var s = document.createElement('span');
-    var createdAt = new Date(profile.created_at);
-    s.innerText = 'Born ' + createdAt.toDateString();
-    s.setAttribute('title', 'Born created by @abraham');
+    s.innerText = 'Born ' + (new Date(response.data.createdAt)).toDateString();
+    s.setAttribute('title', 'Born for Twitter created by @abraham');
     var lnu = profilesOnPage[0].getElementsByClassName('location-and-url');
-    lnu[0].appendChild(d).parentNode.appendChild(s);
+    
+    // TODO: clean up if
+    // Only add divider if there is a URL present
+    if (lnu[0].getElementsByClassName('url')[0].getElementsByTagName('a')[0].getAttribute('href') !== '') {
+      var d = document.createElement('span');
+      d.className = 'divider';
+      d.innerText = '· ';
+      lnu[0].appendChild(d);
+    }
+    
+    lnu[0].appendChild(s);
   } else {
     // TODO: display rate limit errors
     // console.log('Error response');
     // console.log(response);
   }
+}
+
+function getProfileClassName(id) {
+  return 'born-profile-' + id;
 }
